2235f38d037fa7120acfdf1500393e6d37e69f1f
David Martin Vergues <dmvergues@gmail.com>
2025-09-02 00:14:18 +0200

    update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const apiUrl = process.env.REACT_APP_DOCKER_ENV 
   ? 'http://localhost:8000'
-  : 'https://0b6bc3f6-0480-40e8-8c57-3af2460a82da-dev.e1-eu-north-azure.choreoapis.dev/pruebatecnica-david/backend/v1.0';
+  : 'https://0b6bc3f6-0480-40e8-8c57-3af2460a82da-prod.e1-eu-north-azure.choreoapis.dev/pruebatecnica-david/backend/v1.0';
 
 export { apiUrl };
